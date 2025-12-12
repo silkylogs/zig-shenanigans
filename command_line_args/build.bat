912e4a6420292c3e345c289f555727c9d19447b1
silkylogs <silkylogs@gmail.com>
2025-12-12 23:36:49 +0600

    Redact zig location

--- command_line_args/build.bat
+++ command_line_args/build.bat
@@ -1,4 +1,4 @@
 @echo off
 
-"d:\root\software\PL\zig-win64-0.1.1\zig.exe" build-obj main.zig --library c 
+"zig.exe" build-obj main.zig --library c 
 link.exe zig-cache\*.obj kernel32.lib ucrt.lib bufferoverflowU.lib /entry:main
