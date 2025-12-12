821e03805057631249fc1a71272284546f784133
silkylogs <silkylogs@gmail.com>
2025-12-12 23:20:40 +0600

    initial commit

---
+++ command_line_args/main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const io = std.io;
+const os = std.os;
+const printf = io.stdout.printf;
+
+pub fn main() -> %void {
+    var args_list = std.ArrayList(?%[]u8).init(&std.mem.c_allocator);
+    var args = os.args();
+
+populate:
+    const to_append = args.next(&std.mem.c_allocator);
+    if (to_append != null) {
+        %%args_list.append(to_append);
+        goto populate;
+    }
+
+    {var i: usize = 0; while (i < args_list.len) : (i += 1) {
+        %%printf("Arg {}: {}\n", i, args_list.items[i]);
+    }}
+
+    // %%printf("Arg 0: {}\n", args.next(&std.mem.c_allocator));
+    // %%printf("Arg 1: {}\n", args.next(&std.mem.c_allocator));
+    // %%printf("Arg 2: {}\n", args.next(&std.mem.c_allocator));
+    // %%printf("Arg 3: {}\n", args.next(&std.mem.c_allocator));
+    // %%printf("Arg 4: {}\n", args.next(&std.mem.c_allocator));
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Zig shennanigans, 0.1.1 edition
+
+## Command line arguments in Zig 0.1.1
+Build instructions: Run build.bat in the VS2022 developer command prompt.  
+TODO: tell a story
